15a1630943d3d9994c676751bfc49f41ab9d5cb7
paudeldileep <dileepkpaudel@gmail.com>
2020-10-08 22:40:50 +0545

    search UI

--- client/src/components/layout/Home/index.js
+++ client/src/components/layout/Home/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Expensecm from './Expensecm'
-
+import Searchexp from './Searchexp'
 
 const Home = props => {
     return (
         <>
         <Expensecm/>
+        <Searchexp/>
         </>
     )
 }
